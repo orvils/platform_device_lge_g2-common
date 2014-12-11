eb706bac64f43a7560f5517c5d88fb547a0e8ec8
arcardinal <andrewrcardinal@gmail.com>
2014-12-10 20:58:37 -0600

    g2: enable BOARD_CUSTOM_BOOTIMG
    Change-Id: I8b6734d80bcaf656c9cea888b94a694e89162329

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,6 +36,7 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_NO_BOOTLOADER := true
 
 # Kernel information
+BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --tags_offset 0x04800000
